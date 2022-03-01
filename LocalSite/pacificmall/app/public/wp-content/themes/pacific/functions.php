a251d8061a77f46e739a96247cd8925826a90eab
yuta <9010atuy@gmail.copm>
2022-03-02 01:15:23 +0900

    add custom header

---
+++ LocalSite/pacificmall/app/public/wp-content/themes/pacific/functions.php
@@ -0,0 +1,18 @@
+<?php
+
+// カスタムヘッダー
+// ワードプレス機能を有効化する
+add_theme_support(
+    // カスタムヘッダーを追加
+    'custom-header',
+    array(
+        // ヘッダーの幅
+        'width' => 950,
+        // ヘッダーの高さ
+        'height' => 295,
+        // ヘッダーにテキストを表示させない
+        'header-text' => false,
+        // デフォルトイメージのURL
+        'default-image' => '$s/images/top/main_image.png',
+    )
+);
