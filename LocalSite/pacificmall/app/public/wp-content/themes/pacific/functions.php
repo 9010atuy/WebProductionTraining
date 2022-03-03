cd9b57f80f6eda6577e2fe20bd7d43655e4729a7
yuta <9010atuy@gmail.copm>
2022-03-03 22:40:53 +0900

    add eyecatch_image to funcitons.php, add eycatch

--- LocalSite/pacificmall/app/public/wp-content/themes/pacific/functions.php
+++ LocalSite/pacificmall/app/public/wp-content/themes/pacific/functions.php
@@ -24,3 +24,16 @@ register_nav_menus(
         'place_utility' => 'ユーティリティ',
     )
 );
+
+// アイキャッチ有効化
+add_theme_support('post-thumbnails');
+// アイキャッチ画像サイズ設定
+set_post_thumbnail_size(90, 90, true);
+// サイドバー用画像サイズ
+add_image_size('small_thumbnail', 61, 61, true);
+// アーカイブ用画像サイズ
+add_image_size('large_thumbnail', 120, 120, true);
+// サブページヘッダー用画像サイズ設定
+add_image_size('category-image', 658, 113, true);
+// モールイメージ用画像サイズ設定
+add_image_size('pickup-thumbnail', 302, 123, true);
