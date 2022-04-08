6cf28066b9d3078b5accae58f6712a3d5881dde6
yuta <9010atuy@gmail.copm>
2022-04-09 00:32:07 +0900

    fix loading toggle perf

--- AnimalHospital/js/script.js
+++ AnimalHospital/js/script.js
@@ -1,5 +1,4 @@
 $(function () {
-  $('.nav-toggle-menu').hide();
   $('.nav-btn').on('click', function () {
     $(this).toggleClass('active');
     $('.nav-toggle-menu').stop(true).animate({ height: 'toggle' });
