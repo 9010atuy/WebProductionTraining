d78042ff38e665077ed5b0f3cd36fcb122ff2999
yuta <9010atuy@gmail.copm>
2022-06-06 22:15:28 +0900

    done gulp setup

---
+++ MusicSchool/script.bash
@@ -0,0 +1,13 @@
+# create html
+touch top.html
+touch plan.html
+touch blog_list.html
+touch blog_details.html
+touch notice_list.html
+touch notice_details.html
+touch contact_form.html
+touch contact_send.html
+touch 404.html
+touch search.html
+touch result_list.html
+touch result_details.html
