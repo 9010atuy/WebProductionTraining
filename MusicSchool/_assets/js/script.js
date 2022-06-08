8c3898f44b8e574b1d222295106bb1af1f31f58d
yuta <9010atuy@gmail.copm>
2022-06-08 23:18:49 +0900

    update header sp

--- MusicSchool/_assets/js/script.js
+++ MusicSchool/_assets/js/script.js
@@ -2,6 +2,7 @@
 $('.hamburger-trigger').on('click', () => {
   //　要素自体の表示を切り替え
   $('.hamburger-menu').toggle(300);
+  $('.hamburger-bg').toggle(300);
   // クラスのトグル
   $('.hamburger').toggleClass('active');
 });
