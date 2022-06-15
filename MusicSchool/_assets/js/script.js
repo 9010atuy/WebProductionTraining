1307d749cdeb8b43e26a2cd495613c6ba00f7e89
9010atuy <39493991+9010atuy@users.noreply.github.com>
2022-06-16 01:28:26 +0900

    add top about area

--- MusicSchool/_assets/js/script.js
+++ MusicSchool/_assets/js/script.js
@@ -1,8 +1,8 @@
 // ハンバーガー
 $('.hamburger-trigger').on('click', () => {
   //　要素自体の表示を切り替え
-  $('.hamburger-menu').toggle(300);
-  $('.hamburger-bg').toggle(300);
+  $('.c-hamburger-menu').toggle(300);
+  $('.c-hamburger-bg').toggle(300);
   // クラスのトグル
-  $('.hamburger').toggleClass('active');
+  $('.c-hamburger').toggleClass('active');
 });
