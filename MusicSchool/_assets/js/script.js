3d1b99dc2fd55c6c09b6b35ba89f0a44120a2cbf
9010atuy <39493991+9010atuy@users.noreply.github.com>
2022-06-27 21:47:54 +0900

    add to-top btn

--- MusicSchool/_assets/js/script.js
+++ MusicSchool/_assets/js/script.js
@@ -1,3 +1,4 @@
+'use strict';
 // ハンバーガー
 $('.hamburger-trigger').on('click', () => {
   //　要素自体の表示を切り替え
@@ -33,4 +34,42 @@ $(document).ready(function () {
     $(this).toggleClass('active');
     $(this).next($('.accordion-collapse')).slideToggle();
   });
+});
+
+// トップへ戻る
+// フッターの手前で止める
+const goTop = $('.js-go-top');
+const displayHeight = $('#about').offset().top;
+const footerHeight = $('#footer').innerHeight();
+// FV以外で表示
+$(window).scroll(function () {
+  let fixedBtn = $('.js-fixed-btn-pc');
+  // ウィンドウ幅が1000px以下の場合はターゲット変更
+  if ($(window).width() < 1000) {
+    fixedBtn = $('.js-fixed-btn-no-pc');
+  }
+  // 表示開始要素
+  if ($(this).scrollTop() > displayHeight) {
+    // 0.3秒でフェードイン
+    fixedBtn.fadeIn(300);
+  } else {
+    // 0.3秒でフェードアウト
+    fixedBtn.fadeOut(300);
+  }
+  const scrollHeight = $(document).height();
+  const scrollPosition = $(window).height() + $(window).scrollTop();
+  if (scrollHeight - scrollPosition <= footerHeight) {
+    fixedBtn.removeClass('u-pos-fixed');
+    fixedBtn.addClass('u-pos-abs');
+    $('#blog').addClass('u-pos-rel');
+  } else {
+    $('#blog').removeClass('u-pos-rel');
+    fixedBtn.removeClass('u-pos-abs');
+    fixedBtn.addClass('u-pos-fixed');
+  }
+});
+// ボタンがクリックされたら1秒でページトップへ戻る
+goTop.click(function () {
+  $('body, html').animate({ scrollTop: 0 }, 1000);
+  return false;
 });
