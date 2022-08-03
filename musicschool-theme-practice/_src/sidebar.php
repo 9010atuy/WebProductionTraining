4e2634965de13a45fa8be3bd09652ec0b37cd7d0
9010atuy <39493991+9010atuy@users.noreply.github.com>
2022-08-03 22:13:18 +0900

    add recommend

--- musicschool-theme-practice/_src/sidebar.php
+++ musicschool-theme-practice/_src/sidebar.php
@@ -14,18 +14,48 @@
     </div>
     <div class="c-blog-section">
         <p class="c-blog-section-name c-blog-section-name--side">おすすめの記事</p>
-        <a href="" class="c-blog-section__recommend ">
-            <div class="c-blog-section__recommend-img"></div>
-            <p>タイトルが入ります。タイトル</p>
-        </a>
-        <a href="" class="c-blog-section__recommend">
-            <div class="c-blog-section__recommend-img"></div>
-            <p>タイトルが入ります。タイトル</p>
-        </a>
-        <a href="" class="c-blog-section__recommend">
-            <div class="c-blog-section__recommend-img"></div>
-            <p>タイトルが入ります。タイトル</p>
-        </a>
+        <?php
+        $args = array(
+            'posts_per_post' => 3,
+            'post_type' => 'blog',
+            'taxonomy' => 'blog_recommend',
+            'term' => 'on',
+            'orderby' => 'date',
+            'order' => 'DESC'
+        );
+        $the_query = new WP_Query($args);
+        ?>
+        <?php
+        if ($the_query->have_posts()) :
+            while ($the_query->have_posts()) :
+                $the_query->the_post();
+        ?>
+                <a href="<?php echo get_permalink($post->ID); ?>" class="c-blog-section__recommend ">
+                    <div class="c-blog-section__recommend-img">
+                        <?php
+                        $thumbnail_id = get_post_thumbnail_id($post->ID);
+                        $thumb_url = wp_get_attachment_image_src($thumbnail_id, 'small');
+                        if (get_post_thumbnail_id($post->ID)) :
+                        ?>
+                            <img src="<?php echo $thumb_url[0]; ?>" alt="">
+                        <?php
+                        endif;
+                        ?>
+                    </div>
+                    <p>
+                        <?php
+                        if (mb_strlen($post->post_title) > 23) {
+                            $title = mb_substr($post->post_title, 0, 23);
+                            echo $title . '...';
+                        } else {
+                            echo $post->post_title;
+                        }
+                        ?>
+                    </p>
+                </a>
+            <?php endwhile; ?>
+            <?php wp_reset_postdata(); ?>
+        <?php endif; ?>
     </div>
     <div class="c-blog-section c-blog-section--bottom">
         <p class="c-blog-section-name c-blog-section-name--side ">カテゴリー</p>
