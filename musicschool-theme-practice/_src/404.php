f3e3ccb3293e4ae0c4233c1f96d47f1131bbb029
9010atuy <39493991+9010atuy@users.noreply.github.com>
2022-08-21 15:36:43 +0900

    update 404

---
+++ musicschool-theme-practice/_src/404.php
@@ -0,0 +1,14 @@
+<?php get_header(); ?>
+
+<div class="c-headline headline--404">
+    <p class="c-headline-text">404 not fonud</p>
+</div>
+
+<section class="c-message">
+    <div class="content-area content-area--404">
+        <p class="c-message-description">申し訳ございませんが、お探しのページが見つかりませんでした。<br>お探しのページは一時的に表示ができない状態にあるか、移動または削除された可能性があります。</p>
+        <a href="<?php echo esc_url(home_url('/')); ?>" class="c-button c-button--main">ホームへ戻る</a>
+    </div>
+</section>
+
+<?php get_footer(); ?>
