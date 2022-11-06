c516ed2dc9f6812b7ff5fcc038b1d5379b27a1cc
9010atuy <39493991+9010atuy@users.noreply.github.com>
2022-11-06 11:29:04 +0900

    add breadcrumb template

---
+++ MusicSchool2WP/_static/src/template-parts/breadcrumb.php
@@ -0,0 +1,12 @@
+<nav class="c-breadcrumb">
+    <div class="main-wrapper">
+        <ul>
+            <li>
+                <a href="<?php echo get_template_directory_uri(); ?>/index.html">ホーム</a>
+            </li>
+            <li>
+                <a href="<?php echo get_template_directory_uri(); ?>/plan.html">プラン・料金</a>
+            </li>
+        </ul>
+    </div>
+</nav>
